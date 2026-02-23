8a2b9382c13317ceee1b47974b9e780b1365a79b
Jonas <nichtherrjonas@gmail.com>
2026-02-23 16:11:12 +0100

    test

--- data-manager.js
+++ data-manager.js
@@ -6,6 +6,9 @@ const $ = (e) => document.querySelector(e);
 const categoryContainer = $("#category-container");
 const categoryTemplate = $("#category-template")?.content;
 
+const mainContainer = $("#main-content-list");
+const entryTemplate = $("#main-content-entry")?.content;
+
 {
   const categorys = new Set(...Array(data.map((d) => d?.category)));
   categorys.delete(null);
@@ -14,6 +17,21 @@ const categoryTemplate = $("#category-template")?.content;
       const category = categoryTemplate?.cloneNode(1);
       category.querySelector("#label").innerText = String(name);
       return category;
+    }),
+  );
+}
+
+{
+  const entrys = data.map((d) => d.contents).flat();
+  console.log(entrys);
+  mainContainer.replaceChildren(
+    ...entrys.map(({ title, image, length, src }) => {
+      const entry = entryTemplate?.cloneNode(1);
+      entry.querySelector("h2").src = title;
+      entry.querySelector("img").src = image;
+      entry.querySelector("a").href = src;
+      entry.querySelector("#length").innerText = length;
+      return entry;
     }),
   );
 }
